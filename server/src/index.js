773a5222af158f750f3dae84e9b77cfd282086d8
chasethesky79 <seshadri.bharath@yahoo.com>
2024-01-10 00:18:23 -0600

    Added nodemon dependency to project

--- server/src/index.js
+++ server/src/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = 9000
 
-app.get('/', (req, res) => res.send('Hello world'))
+app.get('/', (req, res) => res.send('Hello world here'))
 app.listen(port)
 
 console.log(`[app]: http://localhost:${port}`)
