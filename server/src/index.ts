8ee3571e2105e0ddc8cca1236adba372fd3b86c0
chasethesky79 <seshadri.bharath@yahoo.com>
2024-01-14 11:11:38 -0600

    Implemented changes to delete a listing

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,18 @@
 import express from 'express'
+import { listings } from './listings'
+import bodyParser from 'body-parser'
 
 const app = express()
 const port = 9000
+app.use(bodyParser.json())
 
 app.get('/', (_req, res) => res.send('Hello world here'))
+app.get('/listings', (_req, res) => res.send(listings))
+app.post('/delete-listing', (req, res) => {
+    const { body: { id }} = req;
+    const indexOfListingToDelete = listings.findIndex(listing => listing.id === id)
+    return indexOfListingToDelete === -1 ? res.send('Failed to delete') : res.send(listings.splice(indexOfListingToDelete, 1))
+})
 app.listen(port)
 
 console.log(`[app]: http://localhost:${port}`)
